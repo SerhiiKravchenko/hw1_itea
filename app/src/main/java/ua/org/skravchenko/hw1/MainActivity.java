2ad20275625a8a5e0da02cf20fd31ae02e9acec7
Serhii <srh.kravchenko@gmail.com>
2018-02-15 19:27:25 +0200

    codeStyle Square

--- app/src/main/java/ua/org/skravchenko/hw1/MainActivity.java
+++ app/src/main/java/ua/org/skravchenko/hw1/MainActivity.java
@@ -10,42 +10,39 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        final Button bOne = findViewById(R.id.bOne);
-        final Button bTwo = findViewById(R.id.bTwo);
-        final String sOne = "BTN 1";
-        final String sTwo = "BTN 2";
+    final Button bOne = findViewById(R.id.bOne);
+    final Button bTwo = findViewById(R.id.bTwo);
+    final String sOne = "BTN 1";
+    final String sTwo = "BTN 2";
 
-        bOne.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (bOne.getText().toString().contentEquals(sOne)){
-                    bOne.setText(sTwo);
-                    bOne.setBackgroundColor(Color.WHITE);
-                } else {
-                    bOne.setText(sOne);
-                    bOne.setBackgroundColor(Color.YELLOW);
-                }
-            }
-        });
+    bOne.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        if (bOne.getText().toString().contentEquals(sOne)) {
+          bOne.setText(sTwo);
+          bOne.setBackgroundColor(Color.WHITE);
+        } else {
+          bOne.setText(sOne);
+          bOne.setBackgroundColor(Color.YELLOW);
+        }
+      }
+    });
 
-        bTwo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Random random = new Random();
-                int rNum = random.nextInt(2);
-                if (rNum==0){
-                    bTwo.setText(sTwo);
-                    bTwo.setBackgroundColor(Color.GRAY);
-                } else {
-                    bTwo.setText(sOne);
-                    bTwo.setBackgroundColor(Color.GREEN);
-                }
-            }
-        });
-    }
+    bTwo.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        Random random = new Random();
+        int rNum = random.nextInt(2);
+        if (rNum == 0) {
+          bTwo.setText(sTwo);
+          bTwo.setBackgroundColor(Color.GRAY);
+        } else {
+          bTwo.setText(sOne);
+          bTwo.setBackgroundColor(Color.GREEN);
+        }
+      }
+    });
+  }
 }
